21517eba2e7c96c481d8d56b22a06b84a65ec556
slevinas <slevinas@gmail.com>
2024-01-15 11:33:06 -0500

    input binding composition api

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -68,31 +68,5 @@ const toggleGoal = () => {
 </script>
 
 <style>
-html {
-  font-family: sans-serif;
-}
 
-body {
-  margin: 3rem;
-  text-align: center;
-}
-
-.goal-button {
-  background-color: #4CAF50;
-  /* Green */
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-  transition-duration: 0.4s;
-}
-
-.goal-button:hover {
-  background-color: #45a049;
-}
 </style>
---
+++ src/views/TestingView1.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="prose p-12 bg-red-100 rounded-md w-[65ch]">
+    <section class="container">
+      <h2>{{ userName }}</h2>
+      <h3>{{ age }}</h3>
+      <button @click="setAge">Change Age</button>
+      <div>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+
+      </div>
+
+    </section>
+
+
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+
+
+
+
+</script>
+
+<style></style>
